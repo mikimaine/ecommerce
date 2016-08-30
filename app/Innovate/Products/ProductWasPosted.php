a8f95a0a4ca9fed6a58e18dfae3f929f76947567
Miki Maine <mikias.amdu@gmail.com>
2016-08-30 14:42:56 +0000

    Applied fixes from StyleCI

--- app/Innovate/Products/ProductWasPosted.php
+++ app/Innovate/Products/ProductWasPosted.php
@@ -4,18 +4,15 @@
  * For : INNOVATE E-COMMERCE
  * User: MIKI$
  * Date: 3/18/2016
- * Time: 5:31 PM
+ * Time: 5:31 PM.
  */
-
 namespace Innovate\Products;
 
-
 /**
- * Class ProductWasPosted
- * @package Innovate\Products
+ * Class ProductWasPosted.
  */
-class ProductWasPosted {
-
+class ProductWasPosted
+{
     /**
      * @var Product
      */
@@ -24,10 +21,8 @@ class ProductWasPosted {
     /**
      * @param Product $product
      */
-    function __construct(Product $product)
+    public function __construct(Product $product)
     {
         $this->product = $product;
     }
-
-
 }
